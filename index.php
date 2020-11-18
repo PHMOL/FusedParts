365c45dfc9ac570a995db2c882604d85b663769d
Philipp Moeller <philipp_moeller@hotmail.de>
2020-11-18 14:22:51 +0100

    changed contact form to formspree

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<?php 
-
-include_once("index.html"); 
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "philipp_moeller@hotmail.de";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
-?>
+<?php include_once("index.html");?>
