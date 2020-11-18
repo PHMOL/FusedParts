b1d1ebd171790b92e49e938d22199175b424f0b1
Philipp Moeller <philipp_moeller@hotmail.de>
2020-11-18 12:47:32 +0100

    added contact form

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php include_once("index.html"); ?>
+
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "philipp_moeller@hotmail.de";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
