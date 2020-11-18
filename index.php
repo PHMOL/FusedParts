2c56913893e0ed66d0b95630aa9681f54e9f03b7
Philipp Moeller <philipp_moeller@hotmail.de>
2020-11-18 13:18:51 +0100

    changed contact-form

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php include_once("index.html"); ?>
-
-<?php $name = $_POST['name'];
+<?php 
+/*  include_once("index.html"); */
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
@@ -8,5 +8,6 @@ $recipient = "philipp_moeller@hotmail.de";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Thank You!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";;
 ?>
+
