e0769044d19f9a383aa0f8a84dcd2b84eb86340a
Philipp Moeller <philipp_moeller@hotmail.de>
2020-11-24 15:41:33 +0100

    changed color scheme

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,4 +1,4 @@
-var prevScrollpos = window.pageYOffset;
+/* var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
@@ -7,7 +7,7 @@ window.onscroll = function() {
     document.getElementById("navbar").style.top = "-120px";
   }
   prevScrollpos = currentScrollPos;
-}
+} */
 
 
 /* Loader */
